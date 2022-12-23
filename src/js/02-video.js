40221ed73768075a398a57aaf28878e735f9bebc
Artem <sribnenko@gmail.com>
2022-12-23 13:45:07 +0200

    finished with hm tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,17 @@
 import Player from '@vimeo/player';
-console.log(Player);
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY_LOCAL = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(currentTime) {
+  localStorage.setItem(KEY_LOCAL, JSON.stringify(currentTime));
+}
+
+const currentTime = JSON.parse(localStorage.getItem(KEY_LOCAL));
+const { seconds } = currentTime;
+
+player.setCurrentTime(seconds);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const KEY_LOCAL = 'feedback-form-state';
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  inputEmail: document.querySelector('.feedback-form input'),
+  textArea: document.querySelector('.feedback-form textarea'),
+};
+let dataUser = localStorage.getItem(KEY_LOCAL);
+dataUser = dataUser ? JSON.parse(dataUser) : {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInputFocus, 500));
+
+autoComplite();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  localStorage.clear();
+  refs.form.reset();
+  console.log(dataUser);
+  return dataUser;
+}
+
+function onInputFocus(event) {
+  if (event.target.name === 'email') {
+    dataUser.email = event.target.value;
+  }
+  if (event.target.name === 'message') {
+    dataUser.message = event.target.value;
+  }
+  localStorage.setItem(KEY_LOCAL, JSON.stringify(dataUser));
+}
+
+function autoComplite() {
+  const { email, message } = dataUser;
+  if (email) {
+    refs.inputEmail.value = email;
+  }
+  if (message) {
+    refs.textArea.value = message;
+  }
+}
